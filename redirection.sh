2da262afa3cd5a1c89e15d776e370b9f0017a40a
Dharani <sambana.dharani97@gmail.com>
2024-09-01 19:50:53 +0530

    v1

--- redirection.sh
+++ redirection.sh
@@ -13,7 +13,7 @@ N="\e[0m"
 CHECK_ROOT(){
     if [ $1 -ne 0 ]
     then
-         echo -e "$R please run this script with root priveleages $N" &>>$LOG_FILE
+         echo -e "$R please run this script with root priveleages $N" | tee -a &>>$LOG_FILE
         exit 1
     fi
 }
@@ -21,10 +21,10 @@ CHECK_ROOT(){
 VALIDATE() {
     if [ $1 -ne 0 ]
     then 
-        echo -e " $2  is not $R sucess $N" &>>$LOG_FILE
+        echo -e " $2  is not $R sucess $N" | tee -a &>>$LOG_FILE
         exit 1
     else
-        echo -e " $2 $G sucessfull $N" &>>$LOG_FILE
+        echo -e " $2 $G sucessfull $N" | tee -a &>>$LOG_FILE
     fi
 }
 
@@ -33,7 +33,7 @@ USAGE(){
     exit 1
 }
 USERID=$(id -u)
-echo "script started executing at : $date"
+echo "script started executing at : $(date)" | tee -a &>>$LOG_FILE
 CHECK_ROOT $USERID
 if [ $# -eq 0 ]
 then
@@ -43,13 +43,13 @@ fi
 for package in $@
 do 
     echo $package
-    dnf list installed $package
+    dnf list installed $package &>>$LOG_FILE
     if [ $? -ne 0 ]
     then
-        echo -e "$package is not installed, $G installing it $N " &>>$LOG_FILE
-        dnf install $package -y
-        VALIDATE $? "$package insatllation"
+        echo -e "$package is not installed, $G installing it $N " | tee -a &>>$LOG_FILE
+        dnf install $package -y &>>$LOG_FILE
+        VALIDATE $? "$package insatllation" 
     else
-        echo -e "$package is already $R installed $N" &>>$LOG_FILE
+        echo -e "$package is already $R installed $N" | tee -a &>>$LOG_FILE
     fi
 done
