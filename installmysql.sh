b56f8ce8c9daa70d7f9ff99bdd2ad8c5569310ff
Dharani <sambana.dharani97@gmail.com>
2024-09-01 13:31:08 +0530

    v1

--- installmysql.sh
+++ installmysql.sh
@@ -16,8 +16,14 @@ if [ $USERID -ne 0 ]
 then
     echo "please run this script with root priveleages"
     exit 1
-   
+fi
+
+dnf list installed git
+if [ $? -ne 0 ]
+then 
+    echo "git is not installed, going to install"
+    dnf install git -y
 else
-    echo "you can install but wait"
+    echo "git is already installed, noting to do"
 
 fi 
