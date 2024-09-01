f59ceef0af8386a0e3fde53f50bd3fa93dccec91
Dharani <sambana.dharani97@gmail.com>
2024-09-01 19:35:35 +0530

    v1

--- redirection.sh
+++ redirection.sh
@@ -21,10 +21,10 @@ CHECK_ROOT(){
 VALIDATE() {
     if [ $1 -ne 0 ]
     then 
-        echo " $2  is not $R sucess $N" &>>$LOG_FILE
+        echo -e " $2  is not $R sucess $N" &>>$LOG_FILE
         exit 1
     else
-        echo " $2 $G sucessfull $N" &>>$LOG_FILE
+        echo -e " $2 $G sucessfull $N" &>>$LOG_FILE
     fi
 }
 USERID=$(id -u)
@@ -36,10 +36,10 @@ do
     dnf list installed $package
     if [ $? -ne 0 ]
     then
-        echo "$package is not installed, $G installing it $N " &>>$LOG_FILE
+        echo -e "$package is not installed, $G installing it $N " &>>$LOG_FILE
         dnf install $package -y
         VALIDATE $? "$package insatllation"
     else
-        echo "$package is already $R installed $N" &>>$LOG_FILE
+        echo -e "$package is already $R installed $N" &>>$LOG_FILE
     fi
 done
