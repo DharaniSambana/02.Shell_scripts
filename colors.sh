1f18ce1bae88a2427b2bfdb5ca1cc0e98d72ffd0
Dharani <sambana.dharani97@gmail.com>
2024-09-01 17:32:26 +0530

    v1

---
+++ colors.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+
+CHECK_ROOT(){
+    if [ $1 -ne 0 ]
+    then
+         echo "please run this script with root priveleages"
+        exit 1
+    fi
+}
+
+VALIDATE() {
+    if [ $1 -ne 0 ]
+    then 
+        echo " $2  is not $R sucess $N"
+        exit 1
+    else
+        echo " $2  $G sucessfull $N"
+    fi
+}
+
+USERID=$(id -u)
+CHECK_ROOT $USERID
+dnf list installed mysql
+if [ $? -ne 0 ]
+then
+    echo "Mysql is not installed, installing it "
+    dnf install mysql-server -y
+    VALIDATE $? "MYSQL insatllation"
+else
+    echo "$G mysql $R is already installed"
+fi
+
+dnf list insatlled git
+if [ $? -ne 0 ]
+then
+    echo "git is not installed, installing it "
+    dnf install git -y
+    VALIDATE $? "git insatllation"
+else
+    echo "$G git $R is already installed"
+fi
