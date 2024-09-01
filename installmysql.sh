15577ae53930a34c414879de869ab3dfe763a5d6
Dharani <sambana.dharani97@gmail.com>
2024-09-01 13:46:34 +0530

    v1

---
+++ installmysql.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+
+USERID=$(id -u)
+echo "user ID is: $USERID"
+if [ $USERID -ne 0 ]
+then
+    echo "please run this script with root priveleages"
+    exit 1
+fi
+
+dnf list installed mysqld
+if [ $? -ne 0 ]
+then 
+    echo "mysqld is not installed, going to install"
+    dnf install mysqld -y
+    if [ $? -ne 0 ]
+    then 
+        echo "mysql installation is not sucess"
+        exit 1
+    else
+        echo "mysql installation sucess"
+        echo "starting mysqld service"
+        systemctl start mysqld
+        systemctl enable mysqld
+        systemctl status mysqld
+
+    fi
+else
+    echo "git is already installed"
+    echo "starting mysqld service"
+    systemctl start mysqld
+    systemctl enable mysqld
+    systemctl status mysqld
+
+
+fi 
