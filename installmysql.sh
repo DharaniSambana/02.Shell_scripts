da94c4992d714b6062d020adfdecf1c08c7c996e
Dharani <sambana.dharani97@gmail.com>
2024-09-01 13:23:15 +0530

    V1

--- installmysql.sh
+++ installmysql.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-<<'COMMENTS'
-1-check root user or not 
-2-if root user proceed with script 
-    if proceed -check already installed or not 
-    if intalled inform user 
-    or 
-    install and inform user
-or else throug error
-COMMENTS
+# <<'COMMENTS'
+# 1-check root user or not 
+# 2-if root user proceed with script 
+#     if proceed -check already installed or not 
+#     if intalled inform user 
+#     or 
+#     install and inform user
+# or else throug error
+# COMMENTS
 
-dnf install git -y 
+USERID=$(id -u)
+echo "user ID is: $USERID"
+if [ USERID -ne 0 ]
+then
+    echo "please run this script with root priveleages"
+    exit 1
+   
+else
+    echo "you can install but wait"
+
+fi 
